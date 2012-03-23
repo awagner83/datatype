1e576d464e66f7bfffe6e1868daf6b6ea131a68b
Adam Wagner <awagner83@gmail.com>
2012-03-22 21:34:03 -0400

    incoercible values are now left unchanged instead of raising a TypeError or ValueError. (coerce_value)

--- datatype/coercion.py
+++ datatype/coercion.py
@@ -22,11 +22,22 @@ def coerce_value(datatype, value):
     Example:
         >>> coerce_value("int", "5")
         5
+
+    If coercion is not possible, the incoercible portion is left changed.
+
+    Example:
+        >>> coerce_value(['int'], ['1', '2', 'c'])
+        [1, 2, 'c']
     """
     return walk(datatype, value, coerce_step)
 
 
 def coerce_step(_, datatype, value, __):
     if type(datatype) == str and type(value) in coercable_types:
-        return NewValue(coerce_to[datatype](value))
+        try:
+            return NewValue(coerce_to[datatype](value))
+        except (TypeError, ValueError):
+            # Because a errors in constructing these types is expected,
+            # pass silently
+            pass
 
--- tests/test_coercion.py
+++ tests/test_coercion.py
@@ -6,3 +6,8 @@ def test_coerce_value():
     assert coerce_value(['str'], [1, 2, 3]) == ['1', '2', '3']
     assert coerce_value({'foo': 'float'}, {'foo': 2}) == {'foo': 2.0}
 
+
+def test_coerce_failure():
+    assert coerce_value('int', 'foo') == 'foo'
+    assert coerce_value(['int'], ['1', 2, 'foo']) == [1, 2, 'foo']
+
